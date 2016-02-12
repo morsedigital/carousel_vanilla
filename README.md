ac67f2b2b34f7a6ec87e00f4e4c4574f2ce70367
Ian Hatton <ian@morsedigital.com>
2016-02-12 10:42:58 +0000

    Amends

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # carousel_vanilla
+
+[![Build Status](https://semaphoreci.com/api/v1/ianhatton/carousel_vanilla/branches/master/badge.svg)](https://semaphoreci.com/ianhatton/carousel_vanilla)
--- spec/carousel_spec.js
+++ spec/carousel_spec.js
@@ -1,11 +1,99 @@
+const _ = require("lodash");
 const CarouselClass = require("../src/carousel");
 
+function createCarousel() {
+  let carouselInner = document.createElement("div");
+  let carouselOuter = document.createElement("div");
+
+  carouselInner.className = "carousel-inner";
+  carouselOuter.id = "carousel";
+
+  carouselOuter.appendChild(carouselInner);
+
+  createCarouselItems(carouselInner);
+
+  document.body.appendChild(carouselOuter);
+
+  return carouselOuter;
+}
+
+function createCarouselImageContainers(item, i) {
+  let dataURLDesktop, dataURLMobile, dataURLTablet;
+  let carouselImageContainer = document.createElement("div");
+
+  carouselImageContainer.className = "carousel-image-container";
+
+  dataURLDesktop = "desktop.jpg";
+
+  if(i % 2) {
+    dataURLMobile = "null";
+    dataURLTablet = "null";
+  } else {
+    dataURLMobile = "mobile.jpg";
+    dataURLTablet = "tablet.jpg";
+  }
+
+  carouselImageContainer.setAttribute("data-mobile", dataURLMobile);
+  carouselImageContainer.setAttribute("data-tablet", dataURLTablet);
+  carouselImageContainer.setAttribute("data-desktop", dataURLDesktop);
+
+  carouselImageContainer.style.backgroundImage = "url('" + dataURLDesktop + "')";
+
+  createCarouselOverlays(carouselImageContainer, i);
+
+  item.appendChild(carouselImageContainer);
+}
+
+function createCarouselItems(carouselInner, items = 2) {
+  let item;
+  let range = _.range(1, (items + 1));
+
+  _.forEach(range, (i) => {
+    item = document.createElement("div");
+    item.className = "carousel-item";
+
+    createCarouselImageContainers(item, i);
+
+    carouselInner.appendChild(item);
+  });
+}
+
+function createCarouselOverlays(carouselImageContainer, i) {
+  let carouselOverlay = document.createElement("div");
+  let a = document.createElement("a");
+  let subtitle = document.createElement("h3");
+  let title = document.createElement("h2");
+
+  carouselOverlay.className = "carousel-overlay";
+
+  if(i % 2) {
+    a.href = "http://www.bobdylan.com";
+    a.innerHTML = "Bob Dylan";
+    title.innerHTML = "Bob Dylan's official website"
+    subtitle.innerHTML = "Blind boy grunt"
+
+  } else {
+    a.href = "http://www.paulsimon.com";
+    a.innerHTML = "Paul Simon";
+    title.innerHTML = "Paul Simon's official website"
+    subtitle.innerHTML = "He's a tiny little showbiz man"
+  }
+
+  carouselOverlay.appendChild(title);
+  carouselOverlay.appendChild(subtitle);
+  carouselOverlay.appendChild(a);
+
+  carouselImageContainer.appendChild(carouselOverlay);
+}
+
 describe("carousel", () => {
-  let carousel;
+  let c, carousel;
 
   beforeEach(() => {
+    c = createCarousel();
+
     carousel = new CarouselClass({
-      element: carousel,
+      element: c,
       autoPlay: false,
       itemClass: "carousel-item"
     }, false);
@@ -13,5 +101,26 @@ describe("carousel", () => {
 
   it("should exist", () => {
     expect(carousel).toBeDefined();
+  });
+
+  describe("render function", () => {
+    beforeEach(() => {
+      spyOn(carousel, "_getItems");
+      spyOn(carousel, "_setDefaultSelected");
+
+      carousel.render();
+    });
+
+    it("should call the _getItems function", () => {
+      expect(carousel._getItems).toHaveBeenCalled();
+    });
+
+    it("should call the _setDefaultSelected function", () => {
+      expect(carousel._setDefaultSelected).toHaveBeenCalled();
+    });
+  });
+
+  describe("_init function", () => {
+    
   });
 });
